afdd97615177b29c42d648d203f4562a9546e19a
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-06-13 13:36:28 +0100

    update

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ const app = express();
 
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// bodyParser middleware
+app.use(bodyParser.jason());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// enable cors
+app.use(cors());
