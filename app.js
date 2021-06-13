dee0298a0ca39dff27b571dec27868507ef4a8a6
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-06-13 14:38:08 +0100

    update

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const cors = require('cors');
 
 const app = express();
 
+const poll = require('./routes/poll');
+
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // bodyParser middleware
-app.use(bodyParser.jason());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.jason());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // enable cors
 app.use(cors());
+
+app.use('/poll', poll);
+
+const port = 3000;
+
+//start server
+app.listen(port, console.log(`server started on port ${port}`));
