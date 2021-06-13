11a625adb2b1f3cdd864cdcb16fe543e4053a962
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-06-13 13:20:47 +0100

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
